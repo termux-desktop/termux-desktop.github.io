ace158752830ac5c0cd1ba133565df9c13dd446e
termux-desktop <dev.yisus@hotmail.com>
2022-04-01 19:23:46 -0500

    update pages

--- start/js/main.js
+++ start/js/main.js
@@ -13,7 +13,7 @@ const typed = new Typed('.typed', {
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
 	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
-	loop: true, // Repetir el array de strings
+	loop: false, // Repetir el array de strings
 	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
 	showCursor: true, // Mostrar cursor palpitanto
 	cursorChar: '|', // Caracter para el cursor
